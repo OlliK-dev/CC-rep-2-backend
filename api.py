7d31e508309097ed5bcc5fe853e6f29cf21e5b4b
OlliK-dev <oltsuuka@gmail.com>
2026-03-26 00:14:45 +0200

    testing

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cc-rep-2.onrender.com/"],
+    allow_origins=["https://cc-rep-2.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
